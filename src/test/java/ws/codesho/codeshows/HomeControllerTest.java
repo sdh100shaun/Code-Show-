3f94c40a69aec52021a29eed5b7a27a7cf05fa8b
Shaun Hare <shaun@shaunhare.co.uk>
2012-02-11 13:18:54 +0000

    tests for bamboo:

---
+++ src/test/java/ws/codesho/codeshows/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package ws.codesho.codeshows;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.*;
+
+
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+
+
+public class HomeControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDemo()
+	{
+		Assert.assertTrue(true);
+		
+	}
+}
